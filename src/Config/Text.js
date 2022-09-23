52d4888969afe7428e18fab870bc43e7f2fe1589
Jabie  Cheung <jabie@air-world.io>
2022-09-23 11:58:36 +0800

    updated eng / chi copywriting added link to the halloween logo on the top right of home page

--- src/Config/Text.js
+++ src/Config/Text.js
@@ -1,120 +1,120 @@
 export const textNavbar = {
   tb01: {
     en: "Buy Now!",
-    ch: "",
+    ch: "立即購買！",
   },
   tb02: {
     en: "Ocean Park Halloween 2022",
-    ch: "",
+    ch: "海洋公園哈囉喂2022",
   },
   tb03: {
     en: "About",
-    ch: "",
+    ch: "關於",
   },
   tb04: {
     en: "Utility",
-    ch: "",
+    ch: "用途",
   },
   tb05: {
     en: "Progress",
-    ch: "",
+    ch: "里程",
   },
   tb06: {
     en: "Account",
-    ch: "",
+    ch: "帳號",
   },
   textComingSoon: {
     en: "COMING SEP 30th!",
-    ch: "",
+    ch: "9月30日上架！",
   },
 };
 
 export const textHomeContent = {
   tdes01: {
     en: "Play-to-Earn enabling NFTs debut with Ocean Park Halloween theme",
-    ch: "",
+    ch: "支援「玩住賺」的NFT將於海洋公園「哈囉喂」中首次亮相。",
   },
   tt01: {
     en: "//Ocean Park Hong Kong : Halloween 2022",
-    ch: "",
+    ch: "//海洋公園哈囉喂2022",
   },
   tt02: {
     en: "//What is AiR Citizen?",
-    ch: "",
+    ch: "//甚麼是「元住民」?",
   },
   tc01: {
     en: "Every October in Hong Kong, the Halloween event held by Ocean Park is a talk-of-the-town sensation that brings joyful thrills and adrenaline rush to visitors through a series of scarily-creative and cleverly-crafted Haunted House attractions. This year, Ocean Park is working with AiR Metaverse to deliver the first ever Online + Offline Ocean Park Halloween experience in which visitors can play through a fascinating story BEFORE, DURING, and AFTER the event. A cast of sea animals dress up in wonderful Halloween costumes to become the first of many upcoming AiR Citizen NFT collection!",
-    ch: "",
+    ch: "每年十月，海洋公園「哈囉喂」都成為全城熱話。園內特設一系列兼具創新與驚嚇的鬼屋及設施，為遊客帶來新奇、好玩又刺激的體驗。今年海洋公園與AiR元宇宙推出首個線上線下互動哈囉喂，讓遊客可以在入場前、公園內及離開後都能體驗一個與別不同的萬聖故事。而首批 AiR Citizen NFT已穿起海洋動物的萬聖主題服飾率先登場！",
   },
   tc02: {
     en: "AiR Citizens are NFT-based characters who reside in the virtual world of AiR Metaverse, a unique Online + Offline location-based Metaverse experience for anyone anywhere. Together, AiR Citizens populate the world of AiR and turn the Metaverse into a NFT playground across different brands and creators. NFT hodlers can utilize their AiR Citizens to participate in a great variety of game plays, to go to work at the different NFT-based AiR buildings, to form adventure teams to explore real world locations, and to be used as loyalty qualifications for Play-to-Earn opportunities.",
-    ch: "",
+    ch: "元住民是生活在AiR元宇宙的NFT角色，與大家一同經歷一個獨特的線上線下互動的實地型元宇宙。元住民遍佈AiR世界，並將整個元宇宙變成不同品牌與創作者的NFT遊樂場。擁有者可利用元住民這個NFT參與各式各樣的遊戲、在不同的NFT建築內工作、組成冒險小隊發掘現實中的可能性，並能以之參與將來推出的「玩住賺」任務。",
   },
   tc03: {
     en: "Ocean Park collaborates with AiR Metaverse to deliver “Metaverse-to-Go”, which adds Metaverse experience into the physical theme park for augmented entertainment through Metaverse gateways, accessible with the AiR Metaverse mobile app. Through AiR Metaverse, Ocean Park is extended into digital space in the form of a fantasy game world, where visitors can embark on an Adventure-in-Reality that crosses the boundaries between virtual and physical, to enjoy a journey that is fun and rewarding. In this unique digital world of adventure, visitors can play the roles of Panda care-takers, protectors of sea animals, or even heroes who are to defeat Halloween monsters to save both the virtual and physical worlds!",
-    ch: "",
+    ch: "海洋公園與AiR元宇宙合作推出「Metaverse-to-Go」。只要使用AiR元宇宙手機應用程式，即可以通過「元宇宙通道」將元宇宙體驗拓展至主題樂園中，使海洋公園融入數碼世界，並搖身一變成為結合虛擬與現實的遊戲世界。遊客得以打破虛幻與真實的界限，在兩個世界中盡情冒險，體驗一段快艇而且有意義的歷程。在這個滿有特色與探險的數碼世界中，遊客可以化身成熊貓護理員、海洋守護者，甚或擊退像素生物的英雄，保護現實與虛擬世界。",
   },
   textVisitSite: {
     en: "Visit Official Site",
-    ch: "",
+    ch: "前往官網",
   },
 };
 
 export const textHomeUtility = {
   tt01: {
     en: "//Utility",
-    ch: "",
+    ch: "//用途",
   },
   tdes01: {
     en: "AiR Citizens are hard-working fellows who demonstrate an ever-growing list of utility in the world of AiR:",
-    ch: "",
+    ch: "元住民是AiR世界中，辛勤工作的居民。他們不辭勞苦地拓展AiR元宇宙的，展現出AiR無窮的可能。",
   },
   tc01: {
-    en: "Form a team of AiR Citizens to participate in present and future game plays",
-    ch: "",
+    en: "Enjoy real world loyalty benefits and privileges from Ocean Park including an entrance ticket Buy-one-Get-one-Free",
+    ch: "由海洋公園送出豐富獎賞及禮遇，包括「買一送一」入票門。",
   },
   tc02: {
-    en: "Own AiR Citizens from different collections to be qualified for Play-to-earn missions",
-    ch: "",
+    en: "Form a team of AiR Citizens to participate in Ocean Park Halloween Play-to-Earn game play to earn from a pool of rewards",
+    ch: "集結多個元住民參與「海洋公園哈囉喂」的「玩住賺」任務，贏得各式富豐獎品！",
   },
   tc03: {
-    en: "Enjoy real world loyalty benefits and privileges from partners",
-    ch: "",
+    en: "Own AiR Citizens from different collections to be qualified for future Play-to-earn missions",
+    ch: "持有元住民NFT的人可參與將來「玩住賺」任務。",
   },
   tc04: {
-    en: "Represent the Hodlers as Avatars in the Metaverse",
-    ch: "",
+    en: "Work as care-takers of the virtual space of each Hodler",
+    ch: "元住民作為守護者存在於虛擬世界中。",
   },
   tc05: {
-    en: "Work as care-takers of the virtual space of each Hodler",
-    ch: "",
+    en: "Represent the Hodlers as Avatars in the Metaverse",
+    ch: "元住民是持有者於AiR元宇宙的化身。",
   },
   tc06: {
     en: "Participate in the upcoming Gamefi to earn digital assets",
-    ch: "",
+    ch: "元住民持有者可於未來的Gamefi（遊戲化金融系統）中賺到意想不到的數碼資產。",
   },
 };
 
 export const textHomeProgress = {
   tt01: {
     en: "//Progress",
-    ch: "",
+    ch: "//里程",
   },
   tdate01: {
     en: "2022 August",
-    ch: "",
+    ch: "2022 八月",
   },
   tdate02: {
     en: "2022 September",
-    ch: "",
+    ch: "2022 九月",
   },
   tdate03: {
     en: "2022 October",
-    ch: "",
+    ch: "2022 十月",
   },
   tdate04: {
     en: "2022 Q3",
-    ch: "",
+    ch: "2022 第三季",
   },
   tdate05: {
     en: "On going",
@@ -130,38 +130,38 @@ export const textHomeProgress = {
   },
   tdes01: {
     en: "Ocean Park Metaverse animal care-takers program launch and Panda AnAn memorial in AiR",
-    ch: "",
+    ch: "海洋公園元宇宙動物護理員計劃啟動，並於AiR悼念安安。",
   },
   tdes02: {
     en: "Ocean Park Halloween 2022 NFT public sales as first AiR Citizens",
-    ch: "",
+    ch: "海洋公園「哈囉喂」2022 首次公開發售元住民NFT。",
   },
   tdes03: {
     en: "Ocean Park Halloween 2022 in AiR Metaverse",
-    ch: "",
+    ch: "海洋公園「哈囉喂」2022正式於 AiR亮相。",
   },
   tdes04: {
     en: "AiR Citizens Play-to-Earn exploration game in AiR",
-    ch: "",
+    ch: " AiR的「玩住賺」元住民探索遊戲。",
   },
   tdes05: {
     en: "Additional Ocean Park AiR Citizens Play-to-Earn games in AiR",
-    ch: "",
+    ch: "更多 AiR的「玩住賺」元住民探索遊戲。",
   },
   tdes06: {
     en: "Ocean Park AiR Citizens as loyalty program",
-    ch: "",
+    ch: "海洋公園的 AiR 元住民加盟榮譽會員計劃。",
   },
   tdes07: {
     en: "Metaverse community games for Ocean Park AiR Citizens",
-    ch: "",
+    ch: "舉辦海洋公園的 AiR元住民的元宇宙社區遊戲。",
   },
 };
 
 export const textFooter = {
   tt01: {
     en: "© AiR 2022 ALL RIGHTS RESERVED.  |",
-    ch: "",
+    ch: "© AiR 2022 ALL RIGHTS RESERVED.  |",
   },
   textTerm: {
     en: "Terms",
